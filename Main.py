680f7a989495f122156896699f5bf20573d1e7f6
Pascal <pbienenstein@posteo.de>
2025-01-24 01:34:10 +0100

    first steps with tkinter

--- Main.py
+++ Main.py
@@ -1,5 +1,16 @@
 import tkinter as tk
+
 root = tk.Tk()
-lable1 = tk.Label(root, text="Hallo Welt")
-lable1.pack()
+root.title("Hier steht der Titel")
+root.geometry("400x400")
+root.minsize(width=250, height=250)
+root.maxsize(width=400, height=400)
+root.resizable(width=False, height=True)
+
+label1 = tk.Label(root, text="Label 1", bg="magenta")
+label1.pack(side="top", expand=True, fill="y")
+
+label2 = tk.Label(root, text="Label 2", bg="blue")
+label2.pack(side="top", expand=True, fill="both")
+
 root.mainloop()
